24de78d17bd0722749eee4157ba52648c1340e17
Musferr <syed.musfer@convexinteractive.com>
2025-12-18 22:40:15 +0500

    base url changed

--- voice-admin/src/services/constants.js
+++ voice-admin/src/services/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://10.100.50.44:8001/';
+export const API_BASE_URL = 'http://103.197.46.74:8001/';
 export const AGENT_BASE_URL = 'https://webrtc.convexinteractive.com:8099/';

530cb8deae41e683a0ac6f9706354937865219e8
Musferr <syed.musfer@convexinteractive.com>
2025-12-18 22:51:42 +0500

    local host changed

--- voice-admin/src/services/constants.js
+++ voice-admin/src/services/constants.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL = 'http://103.197.46.74:8001/';
-export const AGENT_BASE_URL = 'https://webrtc.convexinteractive.com:8099/';
+export const AGENT_BASE_URL = 'http://localhost:8099/';

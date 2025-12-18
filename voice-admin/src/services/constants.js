8da07521c20475b662281afe71d77996e127b930
Musferr <syed.musfer@convexinteractive.com>
2025-12-18 23:24:59 +0500

    baseurl

--- voice-admin/src/services/constants.js
+++ voice-admin/src/services/constants.js
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 'http://103.197.46.74:8001/';
 export const AGENT_BASE_URL = 'https://webrtc.convexinteractive.com:8099/';
-export const d_BASE_URL = '/';
+export const d_BASE_URL = '/sdf';
 // cmnt added
